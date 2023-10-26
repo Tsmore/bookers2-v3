0fcbc5d4c7d37db3e54e3bece4ace4c1d809efff
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-10-26 05:22:43 +0000

    微修正もろもろ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     user_path(current_user)
   end
 
-  def after_sign_out_path(resource)
+  def after_sign_out_path_for(resource)
     about_path
   end
 
