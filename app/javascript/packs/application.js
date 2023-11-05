2f22e9dc8f18cd682e89fbc8286fc6cf059b269b
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-11-05 05:37:13 +0000

    raty導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,13 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
 
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
