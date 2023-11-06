4dd2e445a4731c517abbaa38420aef7873980bde
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-11-06 04:26:47 +0000

    tagを複数の単語検索に対応、半角、全角スペース対応

--- app/controllers/tagsearches_controller.rb
+++ app/controllers/tagsearches_controller.rb
@@ -3,7 +3,10 @@ class TagsearchesController < ApplicationController
   def search
     @model = Book
     @word = params[:content]
-    @books = Book.where("category LIKE?","%#{@word}%")
+    words = @word.split(/[\s\u3000]+/)
+    query = words.map { |word| "category LIKE ?" }.join(' OR ')
+    search_terms = words.map { |word| "%#{word}%" }
+    @books = Book.where(query, *search_terms)
     render "tagsearches/tagsearch"
   end
 
