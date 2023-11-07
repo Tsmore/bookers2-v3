5532ba2e1f81aa473a3e74a1b6b68f94a716e9b8
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-11-07 13:42:01 +0000

    users/indexのみフォローの非同期通信化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,13 +3,13 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
-    render 'relastionship'
+    render 'relationship'
   end
 
   def destroy
     @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
-    render 'relastionship'
+    render 'relationship'
   end
 
   def followings
