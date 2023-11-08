d0e57e9110cce8f65c2d86984d41e008355810ad
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-11-08 06:57:48 +0000

    ゲストログインの実装

--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,8 @@ Rails.application.routes.draw do
 
   resources :chats, only: [:create, :destroy, :show]
 
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+
 end
